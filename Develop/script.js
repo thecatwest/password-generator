d9cd4ed66c2507c341edcc623fd77104594f4fc4
TheCatWest <catwestover@outlook.com>
2021-02-22 12:14:05 -0700

    generate password based on selected characters and return to user

--- Develop/script.js
+++ Develop/script.js
@@ -10,24 +10,55 @@ function writePassword() {
     passwordText.textContent = password;
 }
 
-
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Set globals variables, and the type of content they will contain
-var passwordRandom = "";
-var randomCharacter = "";
-var characterAvailable = [];
-var characterCount = [];
-var characterInteger = {};
+var pwRandom = "";
+var charRandom = "";
+var charString = [];
+var pwLength = [];
+var charInteger = {};
 
 function generatePassword() {
   // promt user for password length
-  while ((characterCount) < 8 || (characterCount) > 128) {
-      characterCount = prompt("Please select a password character length of no less than 8 characters and no more than 128 characters.");
-      characterInteger = characterCount
+  while ((pwLength) < 8 || (pwLength) > 128) {
+      pwLength = prompt("Please select a password character length of no less than 8 characters and no more than 128 characters.");
+      charInteger = pwLength
   } 
-  
   return setPassword();
 };
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function setPassword() {
+  // user prompts to determine password characters
+  var upperCaseLetter = confirm("Include uppercase letters?");
+  if (upperCaseLetter === true) {
+      charString = charString + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+
+  var lowerCaseLetter = confirm("Include lowercase letters?");
+  if (lowerCaseLetter === true) {
+      charString = charString + "abcdefghijklmnopqrstuvwxyz";
+  }
+
+  var numbersCharacters = confirm("Include numbers?");
+  if (numbersCharacters === true) {
+      charString = "0123456789";
+  }
+
+  var specialSymbols = confirm("Include special characters?");
+  if (specialSymbols === true) {
+      charString = charString + "!@#$%^&*?";
+  }
+
+  // for loop to generate a password 
+  for (var i = 0; i < pwLength; i++) {
+      var charRandom = Math.floor(Math.random()*charString.length);
+      var pwCharacter = charString[charRandom];
+      pwRandom += pwCharacter;
+  } 
+
+  // return pwRandom to generate random password
+  return pwRandom;
+
+};
